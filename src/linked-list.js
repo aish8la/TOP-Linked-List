721635e6d88c997eeb5aa693eb8d888729cf656d
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-04 21:45:16 +0500

    Add createNode method and append method

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,6 +1,20 @@
-class LinkedList {
+export class LinkedList {
     #head = null;
     #tail = null;
+
+    createNode(value, nextNode) {
+        return new Node(value, nextNode);
+    }
+
+    append(value) {
+        const newNode = this.createNode(value, null);
+        if(this.#tail === null) {
+            this.#head = this.#tail = newNode;
+        } else {
+            this.#tail.nextNode = newNode;
+            this.#tail = newNode;
+        }
+    }
 }
 
 class Node {
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { LinkedList } from "./linked-list.js";
+
+const list = new LinkedList();
+
+list.append("A");
+list.append("B");
