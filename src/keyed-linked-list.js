b338ac6658b5d38c2827130aa4365690ff9e1a56
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-08 01:45:46 +0500

    Add contains key method to KeyedLinkedList

--- src/keyed-linked-list.js
+++ src/keyed-linked-list.js
@@ -86,4 +86,15 @@ class KeyedNode {
         this.#tail = currentNode;
     }
 
+    contains(key) {
+        let currentNode = this.#head;
+    
+        while (currentNode !== null) {
+          if (currentNode.key === key) return true;
+          currentNode = currentNode.nextNode;
+        }
+    
+        return false;
+    }
+
   }
