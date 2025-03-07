b5107d7ce3bb8410de1e9a8a8861beeb52923fbb
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-08 01:44:21 +0500

    Add size, at and pop methods

--- src/keyed-linked-list.js
+++ src/keyed-linked-list.js
@@ -41,6 +41,49 @@ class KeyedNode {
         }
     }
 
+    size() {
+        let currentNode = this.#head;
+        let count = 0;
     
+        while (currentNode !== null) {
+          count++;
+          currentNode = currentNode.nextNode;
+        }
+    
+        return count;
+    }
+
+    at(index) {
+        if (index > this.size() - 1 || index < 0) return null;
+    
+        let currentNode = this.#head;
+        let i = 0;
+    
+        while (index > i) {
+          currentNode = currentNode.nextNode;
+          i++;
+        }
+    
+        return currentNode;
+    }
+    
+    pop() {
+        if (this.#head === null) return;
+    
+        let currentNode = this.#head;
+    
+        if (currentNode.nextNode === null) {
+          this.#head = this.#tail = null;
+          return;
+        }
+    
+        //Iterates until next node is tail node
+        while (currentNode.nextNode.nextNode !== null) {
+          currentNode = currentNode.nextNode;
+        }
+    
+        currentNode.nextNode = null;
+        this.#tail = currentNode;
+    }
 
   }
