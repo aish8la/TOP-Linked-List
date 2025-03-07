2d92b758f6ae7b888125c45f5043d6a4646d1a21
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-08 01:42:07 +0500

    Add method to append and prepend nodes

--- src/keyed-linked-list.js
+++ src/keyed-linked-list.js
@@ -17,4 +17,30 @@ class KeyedNode {
     get tail() {
       return this.#tail;
     }
+
+    createNode(key, value, nextNode) {
+        return new KeyedNode(key, value, nextNode);
+      }
+
+    append(key, value) {
+        const newNode = this.createNode(key, value, null);
+        if (this.#tail === null) {
+        this.#head = this.#tail = newNode;
+        } else {
+        this.#tail.nextNode = newNode;
+        this.#tail = newNode;
+        }
+    }
+
+    prepend(key, value) {
+        const newNode = this.createNode(key, value, this.#head);
+        if (this.#head === null) {
+        this.#head = this.#tail = newNode;
+        } else {
+        this.#head = newNode;
+        }
+    }
+
+    
+
   }
